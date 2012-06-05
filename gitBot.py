f39258a04b0ca6f3fca4cb57e1ead21f520e90b6
Guillaume BINET <gbin@gootz.net>
2012-06-05 16:03:05 +0200

    First workable version

--- gitBot.py
+++ gitBot.py
@@ -1,10 +1,50 @@
 # -*- coding: utf-8 -*-
+import logging
+from threading import Timer
+import threading
 from botplugin import BotPlugin
+from config import CHATROOM_PRESENCE
+from errBot import admin_only
 from jabberbot import botcmd
-from gittools import clone, pull, get_heads_revisions
+from gittools import clone, get_heads_revisions, fetch_all_heads, history_since_rev, git_log, remove_repo
 from utils import human_name_for_git_url
 
+POLLING_TIME = 120
+
 class GitBot(BotPlugin):
+    git_connected = False
+    ggl = threading.Lock()
+
+    def git_poller(self):
+        with self.ggl:
+            logging.debug('Poll the git repos')
+            history_msg = ''
+            new_stuff = False
+            for human_name in self.shelf:
+                initial_state = self.shelf[human_name]
+                initial_state_dict = dict(initial_state)
+                logging.debug('fetch all heads of %s... ' % human_name)
+                fetch_all_heads(human_name)
+                new_state_dict = {head: rev for head, rev in get_heads_revisions(human_name)}
+                history_msg += human_name + ':\n'
+                for head in initial_state_dict:
+                    if initial_state_dict[head] != new_state_dict[head]:
+                        logging.debug('%s: %s -> %s' % (head, initial_state_dict[head].encode("hex"), new_state_dict[head].encode("hex")))
+                        history_msg += '\tBranch ' + head + ':\n'
+                        history_msg += '\t\t'.join(git_log(history_since_rev(human_name, initial_state)))
+                        new_stuff = True
+                self.shelf[human_name] = [(head, sha) for head, sha in new_state_dict.items() if head in initial_state_dict]
+                self.shelf.sync()
+
+            if new_stuff:
+                for room in CHATROOM_PRESENCE:
+                    logging.debug('Send:\n%s' % history_msg)
+                    self.send(room, 'Incoming git changes ...\n' + history_msg, message_type='groupchat')
+
+            self.t = Timer(5.0, self.git_poller)
+            self.t.setDaemon(True) # so it is not locking on exit
+            self.t.start()
+
     @botcmd
     def follow(self, mess, args):
         """ Follow the given git repository url and be notified when somebody commits something on it
@@ -12,27 +52,28 @@ class GitBot(BotPlugin):
         The next optional arguments are the heads to follow.
         If no optional arguments are given, just follow all the heads
         """
+        admin_only(mess)
         args = args.strip().split(' ')
         if len(args) < 1:
             return 'You need at least a parameter'
         git_url = args[0]
-        heads_to_follow = args[1:] if len(args)>1 else None
+        heads_to_follow = args[1:] if len(args) > 1 else None
 
         human_name = human_name_for_git_url(git_url)
+        with self.ggl:
+            if self.shelf.has_key(human_name):
+                fetch_all_heads(human_name)
+                current_entry = self.shelf[human_name]
+            else:
+                human_name = clone(git_url)
+                current_entry = []
 
-        if self.shelf.has_key(human_name):
-            pull(human_name)
-            current_entry = self.shelf[human_name]
-        else:
-            clone(human_name)
-            current_entry = []
+            current_entry_dict = dict(current_entry)
+            current_entry = [pair for pair in get_heads_revisions(human_name) if pair[0] in heads_to_follow or pair[0] in current_entry_dict] if heads_to_follow else get_heads_revisions(human_name)
+            self.shelf[human_name] = current_entry
+            self.shelf.sync()
 
-        current_entry_dict = dict(current_entry)
-        current_entry = [pair for pair in get_heads_revisions(human_name) if pair[0] in heads_to_follow or pair[0] in current_entry_dict] if heads_to_follow else get_heads_revisions(human_name)
-        self.shelf[human_name] = current_entry
-        self.shelf.sync()
-
-        return '\n%s:\n%s' % (human_name, '\t\n'.join([pair[0] for pair in current_entry]))
+            return self.following(None, None)
 
 
     @botcmd
@@ -42,11 +83,39 @@ class GitBot(BotPlugin):
         The next optional arguments are the heads to unfollow.
         If no optional arguments are given, just unfollow the repo completely
         """
-        return 'ok'
+        admin_only(mess)
+        args = args.strip().split(' ')
+        if len(args) < 1:
+            return 'You need a parameter'
+        human_name = str(args[0])
+        heads_to_unfollow = args[1:] if len(args) > 1 else None
+
+        with self.ggl:
+            if not self.shelf.has_key(human_name):
+                return 'I cannot find %s repos' % human_name
+
+            if heads_to_unfollow:
+                self.shelf[human_name] = [(head, sha) for head, sha in self.shelf[human_name] if head not in heads_to_unfollow]
+                self.shelf.sync()
+                return 'Heads %s have been removed from %s' % (','.join(heads_to_unfollow), human_name) + '\n\n' + self.following(None, None)
+
+            remove_repo(human_name)
+            del(self.shelf[human_name])
+            self.shelf.sync()
+            return ('%s has been removed.' % human_name) + '\n\n' + self.following(None, None)
 
 
     @botcmd
     def following(self, mess, args):
         """ Just prints out which git repo the bot is following
         """
-        return 'ok'
+        if not self.shelf:
+            return 'You have no entry, please use !follow to add some'
+        return '\nYou are currently following those repos:\n'.join(['\n%s:\n%s' % (human_name, '\t\n'.join([pair[0] for pair in current_entry])) for (human_name, current_entry) in self.shelf.iteritems()])
+
+    def callback_connect(self):
+        logging.info('Callback_connect')
+        if not self.git_connected:
+            self.git_connected = True
+            logging.info('Start git poller')
+            self.git_poller()
--- gittools.py
+++ gittools.py
@@ -1,20 +1,42 @@
+from datetime import datetime
 from git import *
 from errBot import PLUGIN_DIR
 from utils import human_name_for_git_url
 import os
+import logging
+import shutil
+
 GIT_LOCAL_STORAGE = PLUGIN_DIR + os.sep + 'git_repos' + os.sep
-g = Git()
+
 
 def human_to_path(human_name):
     return GIT_LOCAL_STORAGE + human_name
 
 def clone(url):
     human_name = human_name_for_git_url(url)
+    g = Git()
     g.clone(url, human_to_path(human_name), bare=True)
     return human_name
 
-def pull(human_name):
-    g.pull(human_to_path(human_name))
+def remove_repo(human_name):
+    path = human_to_path(human_name)
+    shutil.rmtree(path)
+
+def fetch_all_heads(human_name):
+    path = human_to_path(human_name)
+    g = Git(path)
+    logging.debug('fetch_all_heads from %s' % path)
+    remote_heads_string = g.ls_remote('origin', heads=True)
+    branches = [line.split('/')[-1] for line in remote_heads_string.split('\n')]
+    repo = Repo(path)
+    origin = repo.remotes.origin
+    result = []
+    for branch in branches:
+        logging.debug('fetching = %s' % branch)
+        result.extend(origin.fetch('%s:%s' % (branch, branch)))
+
+    logging.debug('result = %s' % result)
+    return result
 
 def get_heads_revisions(human_name):
     path = human_to_path(human_name)
@@ -22,14 +44,18 @@ def get_heads_revisions(human_name):
     heads = repo.heads
     return [(h.name, h.commit.binsha) for h in heads]
 
-def history_since_rev(path, previous_heads_revisions):
-    repo = Repo(path)
+def history_since_rev(human_name, previous_heads_revisions):
+    repo = Repo(human_to_path(human_name))
     heads = repo.heads
+    result = []
     for head_name, previous_commit in previous_heads_revisions:
-        head = heads[head_name]
-        log = head.log()
-        log.reverse()
-        for commit in log:
-            if commit.newhexsha == previous_commit:
-                break
-            print head_name, commit
+        commit = heads[head_name].commit
+        while commit.binsha != previous_commit:
+            result.append(commit)
+            logging.debug('%s' % commit.hexsha)
+            commit = commit.parents[0]
+    return result
+
+# Represents a list of commits as a log as a list of strings
+def git_log(commits):
+    return ["%s %20s %20s -- %s" % (commit.hexsha[:6], commit.author.name, datetime.fromtimestamp(commit.committed_date).isoformat() ,commit.summary) for commit in commits]

18a0a2dc2061272988c5c517f6ded8cdf4cfc3e3
Guillaume Binet <gbin@google.com>
2014-12-01 11:32:43 -0800

    errbot compatibility passed 1.7

--- gitBot.py
+++ gitBot.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 import logging
+from datetime import datetime
 from config import CHATROOM_PRESENCE
-from gittools import clone, get_heads_revisions, fetch_all_heads, history_since_rev, git_log, remove_repo
 from errbot.utils import human_name_for_git_url
 
 POLLING_TIME = 600
 
-# Backward compatibility
-from errbot.version import VERSION
-from errbot.utils import version2array
-if version2array(VERSION) >= [1,6,0]:
-    from errbot import botcmd, BotPlugin
-else:
-    from errbot.botplugin import BotPlugin
-    from errbot.jabberbot import botcmd
+from errbot import botcmd, BotPlugin
 
+from git import *
+import os
+import shutil
 
 class GitBot(BotPlugin):
-    min_err_version = '1.4.0'
+    min_err_version = '1.7.0'
 
     def git_poller(self):
         logging.debug('Poll the git repos')
@@ -27,8 +23,8 @@ class GitBot(BotPlugin):
             initial_state = self[human_name]
             initial_state_dict = dict(initial_state)
             logging.debug('fetch all heads of %s... ' % human_name)
-            fetch_all_heads(human_name)
-            new_state_dict = {head: rev for head, rev in get_heads_revisions(human_name)}
+            self.fetch_all_heads(human_name)
+            new_state_dict = {head: rev for head, rev in self.get_heads_revisions(human_name)}
             history_msg = ''
             new_stuff = False
             for head in initial_state_dict:
@@ -37,7 +33,7 @@ class GitBot(BotPlugin):
                     new_stuff = True
 
             if new_stuff:
-                log = git_log(history_since_rev(human_name, initial_state))
+                log = self.git_log(self.history_since_rev(human_name, initial_state))
                 for head in log:
                     if log[head]: # don't log the empty branches
                         history_msg += '  Branch ' + head + ':\n    '
@@ -61,16 +57,83 @@ class GitBot(BotPlugin):
     def _git_follow_url(self, git_url, heads_to_follow):
         human_name = human_name_for_git_url(git_url)
         if self.has_key(human_name):
-            fetch_all_heads(human_name)
+            self.fetch_all_heads(human_name)
             current_entry = self[human_name]
         else:
-            human_name = clone(git_url)
+            human_name = self.clone(git_url)
             current_entry = []
 
         current_entry_dict = dict(current_entry)
-        current_entry = [pair for pair in get_heads_revisions(human_name) if pair[0] in heads_to_follow or pair[0] in current_entry_dict] if heads_to_follow else get_heads_revisions(human_name)
+        current_entry = [pair for pair in self.get_heads_revisions(human_name) if pair[0] in heads_to_follow or pair[0] in current_entry_dict] if heads_to_follow else self.get_heads_revisions(human_name)
         self[human_name] = current_entry
         return self.git_following(None, None)
+
+
+    def human_to_path(self, human_name):
+        return os.path.join(self.plugin_dir, 'git_repos', human_name)
+
+    def clone(self, url):
+        human_name = human_name_for_git_url(url)
+        g = Git()
+        g.clone(url, self.human_to_path(human_name), bare=True)
+        return human_name
+
+    def remove_repo(self, human_name):
+        path = self.human_to_path(human_name)
+        shutil.rmtree(path)
+
+    def fetch_all_heads(self, human_name):
+        path = self.human_to_path(human_name)
+        g = Git(path)
+        logging.debug('fetch_all_heads from %s' % path)
+        remote_heads_string = g.ls_remote('origin', heads=True)
+        branches = [line.split('/')[-1] for line in remote_heads_string.split('\n')]
+        repo = Repo(path)
+        origin = repo.remotes.origin
+        result = []
+        for branch in branches:
+            logging.debug('fetching = %s' % branch)
+            result.extend(origin.fetch('%s:%s' % (branch, branch)))
+
+        logging.debug('result = %s' % result)
+        return result
+
+
+    def get_heads_revisions(self, human_name):
+        path = self.human_to_path(human_name)
+        repo = Repo(path)
+        heads = repo.heads
+        return [(h.name, h.commit.binsha) for h in heads]
+
+    def history_since_rev(self, human_name, previous_heads_revisions):
+        repo = Repo(self.human_to_path(human_name))
+        heads = repo.heads
+        result = {}
+        for head_name, previous_commit in previous_heads_revisions:
+            commit_list = []
+            parent_commits = [heads[head_name].commit,]
+
+            while previous_commit not in (commit.binsha for commit in parent_commits):
+                new_parents = []
+                for commit in parent_commits:
+                    commit_list.append(commit)
+                    if commit.parents:
+                        new_parents.extend(commit.parents)
+                parent_commits = new_parents
+
+            result[head_name] = commit_list
+        logging.debug('%s, found this history_since_rev %s' % (human_name, result))
+        return result
+
+    # Represents a list of commits as a log as a dictionary of list of strings
+    def git_log(self, head_commits):
+        result = {}
+        for head in head_commits:
+            commits = head_commits[head]
+            logging.debug(u'git log of %s' % head)
+            result[head] = [u"%s %20s %20s -- %s" % (commit.hexsha[:6], commit.author.name, datetime.fromtimestamp(commit.committed_date).isoformat(), commit.summary) for commit in commits]
+            logging.debug(u'%s' % result[head])
+        return result
 
     @botcmd(split_args_with = ' ', admin_only = True)
     def git_follow(self, mess, args):
@@ -115,7 +178,7 @@ class GitBot(BotPlugin):
             self[human_name] = [(head, sha) for head, sha in self[human_name] if head not in heads_to_unfollow]
             return 'Heads %s have been removed from %s' % (','.join(heads_to_unfollow), human_name) + '\n\n' + self.git_following(None, None)
 
-        remove_repo(human_name)
+        self.remove_repo(human_name)
         del(self[human_name])
         return ('%s has been removed.' % human_name) + '\n\n' + self.git_following(None, None)
 

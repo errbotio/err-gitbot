c468d4fb000b706c2ce8b89825f24ade13bfe2eb
Raphael Boidol <raphael.boidol@tngtech.com>
2016-02-06 22:01:42 +0100

    use items() instead of iteritems()

--- gitBot.py
+++ gitBot.py
@@ -47,7 +47,7 @@ class GitBot(BotPlugin):
             if CHATROOM_PRESENCE:
                 room = CHATROOM_PRESENCE[0]
                 self.send(room, '/me is about to give you the latest git repo news ...', message_type='groupchat')
-                for repo, changes in history_msgs.iteritems():
+                for repo, changes in history_msgs.items():
                     msg = ('%s:\n' % repo) + changes
                     logging.debug('Send:\n%s' % msg)
                     self.send(room, msg, message_type='groupchat')
@@ -150,7 +150,7 @@ class GitBot(BotPlugin):
         result = ''
         installed_plugin_repos = self.get_installed_plugin_repos()
         if git_name == 'allplugins':
-            for url in [url for _, url in installed_plugin_repos.iteritems()]:
+            for url in [url for _, url in installed_plugin_repos.items()]:
                 result = self._git_follow_url(url, None)  # follow everything
             return result
         elif git_name in installed_plugin_repos:
